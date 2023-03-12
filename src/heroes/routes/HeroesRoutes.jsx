e02d6fa8fffcb111a7f0f45bd144ed373fac7bc0
Angel Antonio Rivillo Diaz <antorivillo@gmail.com>
2023-03-12 20:46:20 +0100

    first commit

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,24 @@
+
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from "../../ui/components/Navbar"
+import { DcPage, MarvelPage } from "../../heroes"
+import { SearchPage } from "../pages/SearchPage"
+import { HeroPage } from "../pages/HeroPage"
+
+export const HeroesRoutes = () => {
+    return (
+        <>
+            <Navbar />
+
+            <div className="container">
+                <Routes>
+                    <Route path="marvel" element={<MarvelPage />} />
+                    <Route path="dc" element={<DcPage />} />
+
+                    <Route path="search" element={<SearchPage />} />
+                    <Route path="hero/:id" element={<HeroPage />} />
+                </Routes>
+            </div>
+        </>
+    )
+}
