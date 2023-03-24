ff4c01146e1f9487fd23bd2bdaa71e59eacbe391
Angel Antonio Rivillo Diaz <antorivillo@gmail.com>
2023-03-24 12:15:49 +0100

    public private routes

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
-
+import { AuthContext } from '../../auth';
 
 export const Navbar = () => {
 
+    const { user, logout } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const onLogout = () => {
+        logout();
         navigate('/login', {
             replace: true
         });
@@ -14,10 +17,7 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
-            <Link
-                className="navbar-brand"
-                to="/"
-            >
+            <Link className="navbar-brand" to="/" >
                 Asociaciones
             </Link>
 
@@ -50,7 +50,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-info">
-                        Angel
+                        {user?.name}
                     </span>
 
                     <button className="nav-item nav-link btn"
